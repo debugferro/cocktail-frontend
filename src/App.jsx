ecee59df20837c3412e436f410d00cfd34320693
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-02-19 21:30:55 -0300

    Saving configs

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Provider } from '@reduxjs/toolkit';
+
+import logo from './logo.svg';
+import '../assets/stylesheets/App.css';
+
+function App() {
+  return (
+    <Provider>
+
+    </Provider>
+  );
+}
+
+export default App;
