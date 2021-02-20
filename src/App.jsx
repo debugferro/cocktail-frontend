dd4498919fec9286d614abe19345b3bf0c9e478f
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-02-20 00:35:26 -0300

    Config/Routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { Provider } from '@reduxjs/toolkit';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import thunk from 'redux-thunk';
 
-import logo from './logo.svg';
-import '../assets/stylesheets/App.css';
+import Index from './components/index';
+import SignUp from './components/signup';
+
+const reducers = combineReducers({
+  // key: reducer
+});
+
+const initialState = {
+
+};
+
+const middlewares = applyMiddleware(thunk);
+const store = createStore(reducers, initialState, middlewares);
 
 function App() {
   return (
-    <Provider>
-
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   );
 }
