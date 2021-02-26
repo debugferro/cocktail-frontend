c3c7fbab38abfd86da777822cb2b045bc488d576
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-02-26 01:19:52 -0300

    Top bar and media queries

---
+++ src/screens/components/top_bar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../styles/components/top_bar.module.css';
+
+function TopBar() {
+  return (
+    <div className={styles.topBar}>
+      <span>Pink</span><span>Lady</span>
+    </div>
+  );
+}
+
+export default TopBar;
--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import styles from '../styles/screens/signup.module.css';
 import SingUpForm from './components/signup_form';
+
 import Logo from './components/logo';
+import TopBar from './components/top_bar';
 
 function SignUp() {
   return (
-    <div className={styles.background}>
-      <div className={styles.container}>
-        <Logo />
-        <div className={styles.card}>
-          <h1>Sign Up</h1>
-          <SingUpForm />
+    <>
+      <TopBar />
+      <div className={styles.background}>
+        <div className={styles.container}>
+          <Logo />
+          <div className={styles.card}>
+            <h1>Sign Up</h1>
+            <SingUpForm />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
