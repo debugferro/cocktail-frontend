16475d84ed79fe8999cf1ca5eceb7638dc01ef85
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-02-26 00:23:00 -0300

    logo

--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from '../styles/screens/signup.module.css';
 import SingUpForm from './components/signup_form';
+import Logo from './components/logo';
 
 function SignUp() {
   return (
     <div className={styles.background}>
       <div className={styles.container}>
+        <Logo />
         <div className={styles.card}>
-          <h1>SignUp</h1>
+          <h1>Sign Up</h1>
           <SingUpForm />
         </div>
       </div>
