b52803228eee1f6768292d3d9016e69de709a132
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-02-27 02:23:15 -0300

    popular cocktails showing

--- src/screens/components/recommended_cocktails.jsx
+++ src/screens/components/recommended_cocktails.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 function RecommendedCocktails() {
-  const username = useSelector((state) => state.username);
+  const username = useSelector((state) => state.user.entity.username);
+  const cocktails = useSelector((state) => state.cocktails.entities.recommended);
+
+  useEffect(() => {
+
+  }, [cocktails]);
 
   return (
     <>
+      { cocktails && cocktails.map((cocktail) => (
+        <p key={cocktail.id}>{cocktail.name}</p>
+        ))
+      }
       <h1>Welcome {username}</h1>
     </>
   );
--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -2,25 +2,19 @@ import React from 'react';
 
 import styles from '../styles/screens/signin_signup.module.css';
 import SingUpForm from './components/signup_form';
-import Logo from './components/logo';
 
 function SignUp() {
   return (
     <>
-
-
       <div className={styles.background}>
-          <div className={styles.closeBtn}>
-            <span>X</span>
-          </div>
-
-          <div className={styles.card}>
-            <h1>Sign Up</h1>
-            <SingUpForm />
-          </div>
+        <div className={styles.closeBtn}>
+          <span>X</span>
+        </div>
+        <div className={styles.card}>
+          <h1>Sign Up</h1>
+          <SingUpForm />
+        </div>
       </div>
-
-
     </>
   );
 }
